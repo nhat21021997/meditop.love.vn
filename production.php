8c0b2a92d59cbcd68e7e4d4890c8241e5ef147cc
nhat21021997 <33777264+nhat21021997@users.noreply.github.com>
2018-06-13 21:17:15 +0700

    Initial commit

---
+++ production.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php require_once 'common/head.php'; ?>
+<script>
+	// var BASE_URL = 'http://ht.inviendong.com/';
+</script>
+</head>
+<body>
+	<?php require_once 'common/header2.php'; ?>
+	<div class="uk-hidden" where='index'></div>
+	<div id="product" class="page-body">
+		<section class="bl-top-header">
+			<div class="uk-container uk-container-center">
+				<div class="bl-head">
+					<h3 class="heading-1">Tin tức</h3>
+				</div>
+				<div class="bl-breadcrumb">
+					<ul class="uk-list uk-clearfix">
+						<li><a href="">Trang Chủ</a></li>
+						<li class="bl-active">Tin tức</li>
+					</ul>
+				</div>
+			</div>
+		</section><!-- bl-top-header -->
+		<section class="bl-main-body">
+			<div class="uk-container uk-container-center">
+				<div class="uk-grid uk-grid-medium">
+					<div class="uk-width-large-1-4 uk-width-medium-1-4 uk-width-small-1-1">
+						<?php require_once 'common/aside-2.php'; ?>
+					</div>
+					<div class="uk-width-large-3-4 uk-width-medium-3-4 uk-width-small-1-1">
+						<section class="detail">
+							<div class="uk-grid uk-grid-small">
+								
+							</div>
+						</section>
+					</div><!-- uk-width-large-3-4 uk-width-medium-3-4 uk-width-small-1-1 -->
+				</div>
+			</div>
+		</section><!-- bl-main-body -->
+		
+	</div><!-- .page-body -->
+	<!-- ---------- -->
+	
+	<?php require_once 'common/footer.php'; ?>
+	<?php require_once 'common/offcanvas.php'; ?>
+	<?php require_once 'common/script.php'; ?>
+</body>
+</html>
---
+++ project.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php require_once 'common/head.php'; ?>
+<script>
+	// var BASE_URL = 'http://ht.inviendong.com/';
+</script>
+</head>
+<body>
+	<?php require_once 'common/header2.php'; ?>
+	<div class="uk-hidden" where='index'></div>
+	<div id="project" class="page-body">
+		
+		<section class="bl-top-header">
+			<div class="uk-container uk-container-center">
+				<div class="bl-head">
+					<h3 class="heading-1">dự án</h3>
+				</div>
+				<div class="bl-breadcrumb">
+					<ul class="uk-list uk-clearfix">
+						<li><a href="">Trang Chủ</a></li>
+						<li class="bl-active">dự án</li>
+					</ul>
+				</div>
+			</div>
+		</section><!-- bl-top-header -->
+		<section class="bl-main-body">
+			<div class="uk-container uk-container-center">
+				<div class="panel-head">
+					<div class="heading-1">Dự án thực hiện bởi Mediatop</div>
+				</div>
+				<div class="panel-body">
+					<ul class="uk-list uk-grid uk-grid-large uk-clearfix list-product">	
+						<li class="uk-width-large-1-2 uk-width-medium-1-2 uk-width-small-1-1">
+							<div class="project">
+								<div class="thumb img-cover">
+									<img src="resources/img/upload/project-sp1.png" alt="">
+								</div>
+								<div class="title"><a href="" title="">VIETNAM MEDICAL EQUIPMENT CORPORATION</a></div>
+								<div class="description">
+									<p>As a leading integrated healthcare solutions organisation in Vietnam, VINAMED is operating in 5 major areas, including: Medical Equipment and Disposable Products Manufacturing, Medical Equipment and Disposable Products Distribution, Medical IT Solutions, Healthcare Facilities Consulting & Construction and Healthcare Investment.</p>
+								</div>
+							</div><!-- project -->
+						</li><!-- khoi 1 -->
+						<li class="uk-width-large-1-2 uk-width-medium-1-2 uk-width-small-1-1">
+							<div class="project">
+								<div class="thumb img-cover">
+									<img src="resources/img/upload/project-sp1.png" alt="">
+								</div>
+								<div class="title"><a href="" title="">VIETNAM MEDICAL EQUIPMENT CORPORATION</a></div>
+								<div class="description">
+									<p>As a leading integrated healthcare solutions organisation in Vietnam, VINAMED is operating in 5 major areas, including: Medical Equipment and Disposable Products Manufacturing, Medical Equipment and Disposable Products Distribution, Medical IT Solutions, Healthcare Facilities Consulting & Construction and Healthcare Investment.</p>
+								</div>
+							</div><!-- project -->
+						</li>
+					</ul>
+					<ul class="uk-list uk-grid uk-grid-large uk-clearfix list-product">	
+						<li class="uk-width-large-1-2 uk-width-medium-1-2 uk-width-small-1-1">
+							<div class="project">
+								<div class="thumb img-cover">
+									<img src="resources/img/upload/project-sp1.png" alt="">
+								</div>
+								<div class="title"><a href="" title="">VIETNAM MEDICAL EQUIPMENT CORPORATION</a></div>
+								<div class="description">
+									<p>As a leading integrated healthcare solutions organisation in Vietnam, VINAMED is operating in 5 major areas, including: Medical Equipment and Disposable Products Manufacturing, Medical Equipment and Disposable Products Distribution, Medical IT Solutions, Healthcare Facilities Consulting & Construction and Healthcare Investment.</p>
+								</div>
+							</div><!-- project -->
+						</li><!-- khoi 1 -->
+						<li class="uk-width-large-1-2 uk-width-medium-1-2 uk-width-small-1-1">
+							<div class="project">
+								<div class="thumb img-cover">
+									<img src="resources/img/upload/project-sp1.png" alt="">
+								</div>
+								<div class="title"><a href="" title="">VIETNAM MEDICAL EQUIPMENT CORPORATION</a></div>
+								<div class="description">
+									<p>As a leading integrated healthcare solutions organisation in Vietnam, VINAMED is operating in 5 major areas, including: Medical Equipment and Disposable Products Manufacturing, Medical Equipment and Disposable Products Distribution, Medical IT Solutions, Healthcare Facilities Consulting & Construction and Healthcare Investment.</p>
+								</div>
+							</div><!-- project -->
+						</li>
+					</ul>
+				</div><!-- panel-body -->
+				<div class="readmore">
+					<a href="" title="">Xem Thêm</a>
+				</div>
+			</div>
+		</section><!-- bl-main-body -->
+		
+	</div><!-- .page-body -->
+	<!-- ---------- -->
+	
+	<?php require_once 'common/footer.php'; ?>
+	<?php require_once 'common/offcanvas.php'; ?>
+	<?php require_once 'common/script.php'; ?>
+</body>
+</html>
